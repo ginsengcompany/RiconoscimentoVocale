1b878dde6b9f121d34bb15140b6c1beec8653c01
FabriGinseng <antoniofabriziofiume@libero.it>
2018-06-20 15:00:14 +0200

    first commit

---
+++ RiconoscimentoVocale/RiconoscimentoVocale/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Plugin.CrossSpeechToText.Stt;
+using Xamarin.Forms;
+
+namespace RiconoscimentoVocale
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+	    private async void IniziaRiconoscimento(object sender, EventArgs e)
+	    {
+	        var result = await CrossSpeechToText.StartVoiceInput("Voice Input!");
+	        txtResult.Text = result;
+        }
+	}
+}
